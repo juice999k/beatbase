39ed146ee289d0f9652e0819669ed430fe392f2e
m <mart.sandovalb@duocuc.cl>
2026-04-14 13:40:56 -0400

    subida de springboot
    primer commit en el cual añadimos nuestro proyecto!

---
+++ src/main/java/cl/duoc/beatbase/BeatbaseApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.beatbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeatbaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeatbaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/duoc/beatbase/BeatbaseApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.beatbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeatbaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

5ba0d3fcbec4bd7bbb344297a25a0365fa0ec889
m <mart.sandovalb@duocuc.cl>
2026-04-15 16:53:16 +0000

    sacar wena

--- src/main/java/cl/duoc/beatbase/BeatbaseApplication.java
+++ src/main/java/cl/duoc/beatbase/BeatbaseApplication.java
@@ -3,7 +3,6 @@ package cl.duoc.beatbase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-#WENA
 	
 @SpringBootApplication
 public class BeatbaseApplication {

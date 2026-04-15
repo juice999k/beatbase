03570e286d98148104948975afe7186fd2c56f94
m <mart.sandovalb@duocuc.cl>
2026-04-15 16:52:52 +0000

    añadir model

---
+++ src/main/java/cl/duoc/beatbase/model/model.java
@@ -0,0 +1,33 @@
+package com.duoc.beatbase.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "artista")
+public class model {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotBlank
+    private String nombre;
+
+    @NotBlank
+    private int edad ;
+
+    @NotBlank
+    private String sexo;
+
+    @NotNull
+    private String genero;
+
+}

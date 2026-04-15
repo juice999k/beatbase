1d79adee07b15807460b0d12d1552784ee666f4b
Vicker10 <vickerlukas@gmail.com>
2026-04-15 12:40:55 -0400

    Add WENA comment to BeatbaseApplication

--- src/main/java/cl/duoc/beatbase/BeatbaseApplication.java
+++ src/main/java/cl/duoc/beatbase/BeatbaseApplication.java
@@ -3,6 +3,8 @@ package cl.duoc.beatbase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+#WENA
+	
 @SpringBootApplication
 public class BeatbaseApplication {
 
